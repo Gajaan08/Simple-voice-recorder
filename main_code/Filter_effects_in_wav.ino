e59861c577a48362300b369757f8358c1c409404
Gajaan08 <81348862+Gajaan08@users.noreply.github.com>
2021-05-06 13:44:48 +0530

    corrected_filtered_effect

--- main_code/Filter_effects_in_wav.ino
+++ main_code/Filter_effects_in_wav.ino
@@ -17,36 +17,35 @@ void setup() {
   }
   read_write() ;  
 }
-
-void loop(){
-  
+void loop(){  
 }
-
 void read_write(){
-   File s_File = SD.open("test.wav");
+     File s_File = SD.open("test.wav");
     s_File.seek(40);
     byte firstB = s_File.read();
     byte nextB = s_File.read();
-    numBytes = firstB + (nextB << 8);
+    byte next1B = s_File.read();
+    byte next2B = s_File.read();
+    numBytes = firstB + (nextB << 8)+(next1B << 16)+(next2B << 24);
     Serial.print("Num Bytes ");
     Serial.println(numBytes);
-    int numByte_selected = 80;
+    //int numByte_selected = numBytes;
+    int numByte_selected = 100;
     Serial.print("numByte_selected");
     Serial.println(numByte_selected);
+    byte waveheader[44];
     s_File.seek(0); 
-    char waveheader[44];
     for (int h = 0; h < 44; h++){
-      //waveheader[h] = s_File.read() ;
-      //Serial.println(s_File.read(),HEX);
-      sprintf(waveheader, "%02X ", s_File.read());
-      //Serial.println(waveheader[h],HEX);
-      }
-    s_File.seek(44); 
-    // storing the data
+      waveheader[h] = s_File.read() ;
+    }
+    // filling empty....
     byte wavecontents[numByte_selected];
+    // storing the data
+    s_File.seek(44);
     for (int i = 0; i < numByte_selected; i++){
       sprintf(wavecontents, "%02X ", s_File.read());  
     } 
+
      // filtering
     byte threshold = 0x10;
     byte filteredwave[numByte_selected];
@@ -65,26 +64,11 @@ void read_write(){
     }
     
     s_File.close();
-//    for (int i = 0; i < numByte_selected; i++){
-//          Serial.println(filteredwave[i]);
-//    }
+    
     Serial.println("Creating ...");
-
     File myFile = SD.open("filter.wav", FILE_WRITE);
-
     myFile.close();
-
-  // Check to see if the file exists:
-
-    if (SD.exists("filter.wav")) {
-
-      Serial.println("filter.wav exists.");
-
-     } else {
-
-        Serial.println("filter.wav doesn't exist.");
-
-      }     
+    
     byte finalfiltereddata[numByte_selected + 44];
     File sFilter = SD.open("filter.wav",FILE_WRITE);
     if (sFilter){
@@ -95,21 +79,31 @@ void read_write(){
       if (m < 44)
       {
         finalfiltereddata[m] = waveheader[m];
-        sFilter.println(finalfiltereddata[m]);
-        //sprintf(finalfiltereddata, "%02X ",waveheader[m] );
       }
       else
       {
        finalfiltereddata[m] = filteredwave[m - 44];
-        sFilter.println(finalfiltereddata[m]); 
-        //sprintf(finalfiltereddata, "%02X ",filteredwave[m - 44] );
       }
     }
+    sFilter.write((byte *)&finalfiltereddata,numByte_selected + 44);
     sFilter.close();
     Serial.println("done");
     } else {
     // if the file didn't open, print an error:
     Serial.println("error opening");
   } 
-  //Serial.print(sizeof(finalfiltereddata));      
+//  for (int i = 0; i < numByte_selected; i++){
+//          Serial.println(filteredwave[i]);
+//    }
+    
+    File sFile = SD.open("filter.wav");
+    if( !sFile ){ 
+        Serial.println("File not opened"); 
+    }
+    else {
+        for (byte i = 0; i <numByte_selected+44 ; i++){
+        
+            Serial.println(sFile.read());
+        }
+    }
 }
